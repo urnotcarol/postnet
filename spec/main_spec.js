21f02ec733474e3ab5074669267016524d96866a
urnotcarol <caorongcarol@163.com>
2017-08-19 14:28:41 +0800

    finish

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,20 +1,44 @@
 "use strict";
-var _ = require("lodash");
-var chai = require("chai");
-var sinon = require("sinon");
-var sinonChai = require("sinon-chai");
-var expect = chai.expect;
+let _ = require("lodash");
+let chai = require("chai");
+let sinon = require("sinon");
+let sinonChai = require("sinon-chai");
+let expect = chai.expect;
 chai.use(sinonChai);
 
-var transformPostcode = require("../lib/main.js");
+let transformPostcode = require("../lib/main.js");
 
 
-describe("postnet", function(){
-    it("returns relevent barcode given 5-digit zipcode", function(){
+describe("postnet", () => {
+    it("returns relevent barcode given 5-digit zipcode", () => {
 
-        var result = transformPostcode("95713");
-        var expect_string = '||:|:::|:|:|:::|:::||::||::|:|:|';
+        let result = transformPostcode("95713");
+        let expectString = "||:|:::|:|:|:::|:::||::||::|:|:|";
         
-        expect(expect_string).to.equal(result);
+        expect(expectString).to.equal(result);
+    });
+
+    it ("returns relevent barcode given 9-digit zipcode", () => {
+        let result = transformPostcode("123451111");
+        let expectString = "|:::||::|:|::||::|::|:|:|::::||:::||:::||:::||:::|||";
+        expect(expectString).to.equal(result);
+    });
+
+    it ("returns relevent barcode given 10-digit zipcode", () => {
+        let result = transformPostcode("12345-1111");
+        let expectString = "|:::||::|:|::||::|::|:|:|::::||:::||:::||:::||:::|||";
+        expect(expectString).to.equal(result);
+    });
+
+    it ("returns relevent zipcode given 5-digit barcode", () => {
+        let result = transformPostcode("||:|:::|:|:|:::|:::||::||::|:|:|");
+        let expectString = "95713";
+        expect(expectString).to.equal(result);
+    });
+
+    it ("returns relevent zipcode given 9-digit barcode", () => {
+        let result = transformPostcode("|:::||::|:|::||::|::|:|:|::::||:::||:::||:::||:::|||");
+        let expectString = "12345-1111";
+        expect(expectString).to.equal(result);
     });
 });
